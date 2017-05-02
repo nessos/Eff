25570405194bcfd0bc013f75e66e1131ef2a8f58
palladin <nick.palladinos@gmail.com>
2017-05-02 17:30:37 +0300

    Init Eff tests.

---
+++ src/Eff.Tests/EffTests.cs
@@ -0,0 +1,25 @@
+﻿using Eff.Core;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xunit;
+
+namespace Eff.Tests
+{
+    public class EffTests
+    {
+        [Fact]
+        public void SimpleReturn()
+        {
+            async Eff<int> Foo(int x)
+            {
+                return x + 1;
+            }
+
+            Assert.Equal(2, Foo(1).Result);
+        }
+
+    }
+}
--- src/Eff.Tests/Program.cs
+++ src/Eff.Tests/Program.cs
@@ -4,7 +4,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace Eff.Demo
+namespace Eff.Tests
 {
     class Program
     {
