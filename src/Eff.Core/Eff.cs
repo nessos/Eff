ec81a3f38941399bfbedcb0da21bb30a6e8b7e44
palladin <nick.palladinos@gmail.com>
2017-05-02 16:55:49 +0300

    Add basic Eff type.

--- src/Eff.Core/Eff.cs
+++ src/Eff.Core/Eff.cs
@@ -6,7 +6,40 @@ using System.Threading.Tasks;
 
 namespace Eff.Core
 {
-    public class Eff
+    public struct Eff<TResult> 
     {
+        
+        private readonly Task<TResult> task;
+        private readonly TResult result;
+
+        
+        public Eff(TResult result)
+        {
+            this.task = null;
+            this.result = result;
+        }
+
+
+        public Eff(Task<TResult> task)
+        {
+            this.task = task;
+            this.result = default(TResult);
+        }
+
+        public Task<TResult> AsTask() => task ?? Task.FromResult(result);
+        
+        public bool IsCompleted => task == null || task.IsCompleted; 
+        public bool IsFaulted => task == null || task.IsFaulted;
+        public bool IsCanceled => task != null && task.IsCanceled;
+
+        public TResult Result => task == null ? result : task.Result;
+
+        public AggregateException Exception => task != null ? task.Exception : null;
     }
+
+
+
+
+        
+    
 }
