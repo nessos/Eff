ecd490ae114a919f84bb9fb4d791c7467ab37b64
palladin <nick.palladinos@gmail.com>
2017-05-16 19:32:57 +0300

    Add DefaultEffectHandler.

---
+++ src/Eff.Core/DefaultEffectHandler.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Eff.Core
+{
+    public class DefaultEffectHandler : EffectHandler
+    {
+        public override async ValueTask<ValueTuple> Handle<TResult>(IEffect<TResult> effect)
+        {
+            return ValueTuple.Create();
+        }
+
+        public override async ValueTask<ValueTuple> Log(ExceptionLog log)
+        {
+            return ValueTuple.Create();
+        }
+
+        public override async ValueTask<ValueTuple> Log(ResultLog log)
+        {
+            return ValueTuple.Create();
+        }
+    }
+
+
+}
