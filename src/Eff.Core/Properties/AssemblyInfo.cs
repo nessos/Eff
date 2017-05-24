7ba5f112f6218dd6a029736e02cbd7fcf3e98526
palladin <nick.palladinos@gmail.com>
2017-05-24 15:13:35 +0300

    Update AssemblyInfo and add nuspec.

--- src/Eff.Core/Properties/AssemblyInfo.cs
+++ src/Eff.Core/Properties/AssemblyInfo.cs
@@ -5,14 +5,12 @@ using System.Runtime.InteropServices;
 // General Information about an assembly is controlled through the following
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("Eff.Core")]
-[assembly: AssemblyDescription("")]
-[assembly: AssemblyConfiguration("")]
-[assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("Eff.Core")]
+[assembly: AssemblyTitle("Eff")]
+[assembly: AssemblyDescription("Programming with effects and handlers in C#")]
+[assembly: AssemblyCompany("Nessos Information Technologies")]
+[assembly: AssemblyProduct("Eff")]
 [assembly: AssemblyCopyright("Copyright ©  2017")]
-[assembly: AssemblyTrademark("")]
-[assembly: AssemblyCulture("")]
+
 
 // Setting ComVisible to false makes the types in this assembly not visible
 // to COM components.  If you need to access a type in this assembly from
@@ -32,5 +30,5 @@ using System.Runtime.InteropServices;
 // You can specify all the values or you can default the Build and Revision Numbers
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("1.0.0.0")]
-[assembly: AssemblyFileVersion("1.0.0.0")]
+[assembly: AssemblyVersion("0.1")]
+[assembly: AssemblyFileVersion("0.1")]
