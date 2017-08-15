0fb725aa368b7212817288baeb8bc80b46c5b11b
Nick Palladinos <nick.palladinos@gmail.com>
2017-08-15 21:30:58 +0300

    Add NonDeterminism example.

---
+++ src/examples/Eff.Examples.NonDeterminism/Effects.cs
@@ -0,0 +1,44 @@
+﻿using Eff.Core;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Eff.Examples.NonDeterminism
+{
+
+    public static class Effect
+    {
+        public static NonDetEffect<T> Choose<T>(T[] choices,
+                                            [CallerMemberName] string memberName = "",
+                                            [CallerFilePath] string sourceFilePath = "",
+                                            [CallerLineNumber] int sourceLineNumber = 0,
+                                            bool captureState = false)
+        {
+            return new NonDetEffect<T>(choices, memberName, sourceFilePath, sourceLineNumber, captureState);
+        }
+
+        public static List<TResult> Run<TResult>(this Eff<TResult> eff)
+        {
+            switch (eff)
+            {
+                case SetException<TResult> setException:
+                    throw setException.Exception;
+                case SetResult<TResult> setResult:
+                    return new List<TResult> { setResult.Result };
+                case Delay<TResult> delay:
+                    return Run(delay.Func(delay.State));
+                case Await<TResult> awaitEff:
+                    var handler = new NonDetHandler<TResult>(awaitEff.Continuation);
+                    var effect = awaitEff.Effect;
+                    effect.SetState(awaitEff.State);
+                    var _ = effect.Accept(handler).Result;
+                    return handler.Results;
+                default:
+                    throw new NotSupportedException($"{eff.GetType().Name}");
+            }
+        }
+    }
+}
---
+++ src/examples/Eff.Examples.NonDeterminism/NonDetEffect.cs
@@ -0,0 +1,26 @@
+﻿using Eff.Core;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Threading;
+
+namespace Eff.Examples.NonDeterminism
+{
+    public class NonDetEffect<T> : Effect<T>
+    {
+
+        private readonly T[] choices;
+        public NonDetEffect(T[] choices, 
+                            string memberName, string sourceFilePath, int sourceLineNumber, bool captureState)
+            : base(memberName, sourceFilePath, sourceLineNumber)
+        {
+            this.choices = choices;
+        }
+
+        public T[] Choices => choices;
+
+    }
+
+}
