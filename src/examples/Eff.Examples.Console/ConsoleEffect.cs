2466646cb75c265eb2beb2afadd4d320347ea4e9
palladin <nick.palladinos@gmail.com>
2018-01-29 16:59:59 +0200

    Add Console example.

---
+++ src/examples/Eff.Examples.Console/ConsoleEffect.cs
@@ -0,0 +1,26 @@
+﻿using Eff.Core;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Threading;
+
+namespace Eff.Examples.Config
+{
+    public class ConfigEffect : Effect<string>
+    {
+
+        private readonly string key;
+        public ConfigEffect(string key, 
+                            string memberName, string sourceFilePath, int sourceLineNumber, bool captureState)
+            : base(memberName, sourceFilePath, sourceLineNumber)
+        {
+            this.key = key;
+        }
+
+        public string Key => key;
+
+    }
+
+}
