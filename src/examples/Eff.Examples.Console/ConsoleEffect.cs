d5aa4e32f62a1e9fdffa2efe6f05660bbc8ba865
palladin <nick.palladinos@gmail.com>
2018-01-29 20:00:11 +0200

    Add ConsoleEffect.

--- src/examples/Eff.Examples.Console/ConsoleEffect.cs
+++ src/examples/Eff.Examples.Console/ConsoleEffect.cs
@@ -5,22 +5,51 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Threading;
+using System.Runtime.CompilerServices;
 
-namespace Eff.Examples.Config
+namespace Eff.Examples.Console
 {
-    public class ConfigEffect : Effect<string>
+    public class ConsolePrintEffect : Effect<ValueTuple>
     {
 
-        private readonly string key;
-        public ConfigEffect(string key, 
+        private readonly string message;
+        public ConsolePrintEffect(string message, 
                             string memberName, string sourceFilePath, int sourceLineNumber, bool captureState)
             : base(memberName, sourceFilePath, sourceLineNumber)
         {
-            this.key = key;
+            this.message = message;
         }
 
-        public string Key => key;
+        public string Message => message;
 
+    }
+
+    public class ConsoleReadEffect : Effect<string>
+    {
+        public ConsoleReadEffect(string memberName, string sourceFilePath, int sourceLineNumber, bool captureState)
+            : base(memberName, sourceFilePath, sourceLineNumber)
+        {
+        }
+    }
+
+    public static class Effect
+    {
+        public static ConsolePrintEffect Print(string message,
+                                            [CallerMemberName] string memberName = "",
+                                            [CallerFilePath] string sourceFilePath = "",
+                                            [CallerLineNumber] int sourceLineNumber = 0,
+                                            bool captureState = false)
+        {
+            return new ConsolePrintEffect(message, memberName, sourceFilePath, sourceLineNumber, captureState);
+        }
+
+        public static ConsoleReadEffect Read([CallerMemberName] string memberName = "",
+                                             [CallerFilePath] string sourceFilePath = "",
+                                             [CallerLineNumber] int sourceLineNumber = 0,
+                                             bool captureState = false)
+        {
+            return new ConsoleReadEffect(memberName, sourceFilePath, sourceLineNumber, captureState);
+        }
     }
 
 }
