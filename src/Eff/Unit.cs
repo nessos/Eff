fd4b60d5f6b740552dde46d7a4903694f9b494b3
Eirik Tsarpalis <eirik.tsarpalis@gmail.com>
2020-07-11 21:56:45 +0100

    add comments

--- src/Eff/Unit.cs
+++ src/Eff/Unit.cs
@@ -5,6 +5,10 @@ namespace Nessos.Effects
     /// <summary>
     ///   A type inhabited by a single value.
     /// </summary>
+    /// <remarks>
+    ///   While similar to <see cref="void"/>, the unit type is inhabited 
+    ///   by a single materialized value and can be used as a generic parameter.
+    /// </remarks>
     public readonly struct Unit : IEquatable<Unit>
     {
         public static Unit Value => new Unit();
